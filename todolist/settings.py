c2681170c08781be7fd378d27266eae9bc13f4e7
amir <amiralimsaeed@gmail.com>
2025-07-24 18:39:08 +0330

    Initial commit

--- todolist/settings.py
+++ todolist/settings.py
@@ -85,17 +85,14 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 import os
 import dj_database_url
 
-if os.getenv('RENDER'):
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),  # اینجا مقدار متغیر محیطی را به طور صریح می‌دهیم
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
