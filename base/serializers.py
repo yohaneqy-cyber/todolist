7f956240bd50d098231f178ea56661f0b7da60aa
amir <amiralimsaeed@gmail.com>
2025-08-09 09:32:35 +0330

    git to meet

--- base/serializers.py
+++ base/serializers.py
@@ -7,7 +7,10 @@ class TaskSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ChatMessageSerializers(serializers.ModelSerializer):
+    sender_email = serializers.EmailField(source='sender.email', read_only=True)
+    receiver_email = serializers.EmailField(source='receiver.email', read_only=True)
+
     class Meta:
         model = ChatMessage
-        fields = ['id', 'sender', 'receiver', 'message', 'timestamp', 'edited']
+        fields = ['id', 'sender', 'sender_email', 'receiver', 'receiver_email', 'message', 'timestamp', 'edited']
         read_only_fields = ['id', 'sender', 'receiver', 'timestamp', 'edited']
